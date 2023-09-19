2b058498103d6ad4aa823f3ff4d0ab555d04e4c6
Natassa <112552395+natko22@users.noreply.github.com>
2023-09-19 13:54:24 +0200

    add images for photo gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import myImage from "../assets/bd04ad_ce107648610c45c9b6e5704d81900a7c~mv2.webp";
+import Image from "../assets/bd04ad_0c220c5c2b02435f9ecc657b22f0c749~mv2.webp";
 
 function Gallery() {
   return (
     <div>
       <img className="img-gallery" src={myImage} alt="afro-woman" />
+      <img className="img-gallery" src={Image} alt="afro-woman" />
     </div>
   );
 }
