391944a44a3bdeb23e2c985338379f49bdc2f21a
Natassa <112552395+natko22@users.noreply.github.com>
2023-09-20 10:24:25 +0200

    delete images

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import myImage from "../assets/bd04ad_ce107648610c45c9b6e5704d81900a7c~mv2.webp";
-import Image from "../assets/bd04ad_0c220c5c2b02435f9ecc657b22f0c749~mv2.webp";
+import ImageGallery from "react-image-gallery";
 
 function Gallery() {
   return (
     <div>
-      <img className="img-gallery" src={myImage} alt="afro-woman" />
-      <img className="img-gallery" src={Image} alt="afro-woman" />
+      <h2>Bridal Makeup</h2>
     </div>
   );
 }
