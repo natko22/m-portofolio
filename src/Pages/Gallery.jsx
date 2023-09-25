664a531c3c0075cf39e1c65e61e9f1b04b70766b
Natassa <112552395+natko22@users.noreply.github.com>
2023-09-25 14:36:33 +0200

    add react image gallery

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,14 +1,12 @@
+// Gallery.js
 import React from "react";
-import Bridal from "../Components/Bridal"; // Import your Bridal component
+import Bridal from "../Components/Bridal";
 
 function Gallery() {
   return (
     <div>
-      <h2>My Makeup Gallery</h2>
-      {/* Bridal Makeup Section */}
-      <h3>Bridal Makeup</h3>
-      <Bridal /> {/* Render the Bridal component here */}
-      {/* Add other sections as needed */}
+      <h2> Gallery</h2>
+      <Bridal />
     </div>
   );
 }
