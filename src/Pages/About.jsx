d69204f00cd4dfa2bf3ebd50f7b63360be354724
Natassa <112552395+natko22@users.noreply.github.com>
2023-09-30 18:36:00 +0200

    styled audiovsual page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -13,7 +13,10 @@ function About() {
           Freelance Make Up Artist based in Berlin and Greece.
         </h3>
         <h3 className="aboutme-h3 centered">Available worldwide 🌍</h3>
-        <p className="aboutme-p"></p>
+
+        <p className="aboutme-p">
+          Professional Make Up Artist studied at I.I.E.K. MORFI, KEPANSI
+        </p>
       </div>
     </div>
   );
--- src/Pages/Videos.jsx
+++ src/Pages/Videos.jsx
@@ -10,6 +10,8 @@ function Videos() {
         <source src={myVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      <Link>www.ramarexclusive.com</Link>
+
       <video width="640" height="360" controls>
         <source src={video2} type="video/mp4" />
         Your browser does not support the video tag.
