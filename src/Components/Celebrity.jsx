82ef3fcb83119489e5fc2e74f1c1f3a29b2821c9
Natassa <112552395+natko22@users.noreply.github.com>
2023-10-07 13:05:21 +0200

    add celebrity component

---
+++ src/Components/Celebrity.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import LightGallery from "lightgallery/react";
+import "lightgallery/css/lightgallery.css";
+
+function Celebrity() {
+  const importAll = (r) => r.keys().map(r);
+  const celebrityImages = importAll(
+    require.context(
+      "../assets/celebrity",
+      false,
+      /\.(jpg|jpeg|png|NEF|webp|JPG|PNG)$/
+    )
+  );
+
+  const images = celebrityImages.map((image, index) => ({
+    id: index,
+    original: image,
+  }));
+
+  return (
+    <div className="gallery-container">
+      <h2>CELEBRITY MAKE UP</h2>
+      <LightGallery speed={500} download={false}>
+        {images.map((image) => (
+          <a key={image.id} href={image.original} className="gallery-item">
+            <img
+              alt={`Bridal Makeup ${image.id + 1}`}
+              src={image.original}
+              className="gallery-image"
+              loading="lazy"
+            />
+          </a>
+        ))}
+      </LightGallery>
+    </div>
+  );
+}
+
+export default Celebrity;
--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -4,11 +4,13 @@ import Bridal from "../Components/Bridal";
 import Creative from "../Components/Creative";
 import Editorial from "../Components/Editorial";
 import Beauty from "../Components/Beauty";
+import Celebrity from "../Components/Celebrity";
 
 function Gallery() {
   return (
     <div>
       <Editorial />
+      <Celebrity />
       <Bridal />
       <Creative />
       <Beauty />
